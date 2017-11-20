d4810e5fa562ba7b3b6df41779c966915fd4cd9f
Sam <sjoseph1848@gmail.com>
2017-11-19 21:35:20 -0600

    Add project files.

---
+++ UdemyObj5/Archer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyObj5
+{
+    class Archer//template used for modelling and making archer objects
+    {
+        //normally we count arrows as 2,3,4 not 3.458
+        private int arrowCount; //instance variable for storing numbers of arrows in quiver
+        public Archer(int count) => arrowCount = count; //set value of instance variable for each archer object we make
+        //public means accessible anywhere 
+        //int means that it returns a value
+        //this method returns the number of arrows LEFT in the quiver after each shot
+        public int Shoot()
+        {
+            //--arrowCount first the value of the arrow count is reduced by 1 and that new updated value is return
+            //the new value of arrow count is returned to the place where the Shoot method is called
+            if (arrowCount <= 0)//if the value of arrow count is 0 or less, return 0 and stop
+                return 0;
+            return --arrowCount;
+        }
+        //1.add a new method called ReloadQuiver
+        //2.make sure you add a button that actually reloads the quiver
+        public void ReloadQuiver()
+        {
+            arrowCount = 10; //simply assign a new value to the arrow Count variable
+        }
+    }
+}
---
+++ UdemyObj5/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdemyObj5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //put code to run when the button is clicked
+            //we  want the button to fire an arrow
+            //arch.shoot() has the action of shoorting an arrow
+            //it causes the arrow count for the archer object to be decreased by 1
+            //each call to Shoot gives back a number we can here to display number of arrows left.
+            label1.Text = $"Arrows Left:{arch.Shoot()}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            arch.ReloadQuiver(); //now invoke ReloadQuiver on the archer object
+            
+        }
+    }
+}
